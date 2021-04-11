da995a57a8282a4f1e665747b9595b5a41028cd8
Ruchita Gokani <ruchitajobanputra@gmail.com>
2021-04-11 05:01:15 -0400

    modify remove_duplicates problem to add alternate way

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -6,4 +6,12 @@ for i in range(len(int_list)):
     if int_list[i] not in new_list:
         new_list.append(int_list[i])
 
-print(new_list)
+print("change with new list ",  new_list)
+
+for i in range(len(int_list)):
+    try:
+        if int_list[i] == int_list[i+1]:
+            int_list.remove(int_list[i])
+    except IndexError:
+        break
+print("Change with the same list ", int_list)
