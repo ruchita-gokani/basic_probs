b5074132e28fadb126f671fb17be176608582f0b
Ruchita Gokani <ruchitajobanputra@gmail.com>
2021-04-11 04:50:56 -0400

    find palindrome problem

--- find_palindrome.py
+++ find_palindrome.py
@@ -4,13 +4,20 @@
 def palindrome_string(test_string):
     test_list = test_string.split(' ')
     for word in test_list:
+        # if word == word[::-1]:
+        #     return True
         new_word = ''
-        for i in range(len(word)):
-            new_word = new_word + word[len(word)-1-i]
+        n = len(word)
+        for i in range(n):
+            new_word = new_word + word[n-1-i]
+
         if new_word == word:
             return "string contains palindrome"
 
 
 if __name__ == '__main__':
     sentence = input("Enter a sentence for testing the code: ")
-    print(palindrome_string(sentence))
+    if palindrome_string(sentence):
+        print("String has a palindrome")
+    else:
+        print("String does not have a palindrome")
