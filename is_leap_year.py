d8354b2b158711af9df8c753d61cc128f56e8849
Ruchita Gokani <ruchitajobanputra@gmail.com>
2021-04-13 18:25:59 -0400

    New problem code

---
+++ is_leap_year.py
@@ -0,0 +1,15 @@
+"""Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True,
+otherwise return False."""
+
+
+def is_leap(year):
+    leap = False
+    if year%100==0 and year%400==0:
+        leap = True
+    if year%4==0 and year%100 != 0:
+        leap = True
+    return leap
+
+
+year = int(input())
+print(is_leap(year))
---
+++ minion_game.py
@@ -0,0 +1,37 @@
+"""Kevin and Stuart want to play the 'The Minion Game'.
+
+Game Rules
+
+Both players are given the same string,S .
+Both players have to make substrings using the letters of the string S.
+Stuart has to make words starting with consonants.
+Kevin has to make words starting with vowels.
+The game ends when both players have made all possible substrings.
+
+Scoring
+A player gets +1 point for each occurrence of the substring in the string S.
+https://www.hackerrank.com/challenges/the-minion-game/problem"""
+def minion_game(string): #BANANA
+    k = 0
+    s = 0
+    N = len(string) #6
+
+    vowels = "AEIOU"
+    for i in range(N):
+        if string[i] in vowels:
+            k += (N-i)
+        else:
+            s += (N-i)
+    if k > s:
+        print("Kevin", k)
+    elif s > k:
+        print("Stuart", s)
+    else:
+        print("Draw")
+    return
+
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
+
