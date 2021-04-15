f699d4cc8eb68c3322ea7bc7eaf85f1af21b93cf
Ruchita Gokani <ruchitajobanputra@gmail.com>
2021-04-15 18:10:34 -0400

    SSH and get IP addresses from host. using subprocess and regex

---
+++ get_ip_addresses.py
@@ -0,0 +1,22 @@
+"""Given a hostname as input. print all the IP addresses of the host. Assume SSH keys are in place for root user.
+# ./getIPaddress.py web1.facebook.com
+# 127.0.0.1
+# 10.3.3.4
+# 192.168.0.12"""
+import subprocess
+import re
+import sys
+
+
+def getIPaddress(hostname):
+
+    command = ["ssh", hostname, "ifconfig", "|", "grep", "inet"]
+    r = subprocess.run(command, capture_output=True, text=True)
+    pattern = re.compile(r'(?:inet)+\s([0-9]{1,3}\.){3}[0-9]{1,3}')
+    matches = pattern.finditer(str(r.stdout))
+    for match in matches:
+        print(match.group(0))
+
+if __name__=='__main__':
+    hname = str(sys.argv[1]),
+    getIPaddress(hname)
