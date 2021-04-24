e5a9e6cef72b4fd8c648febd73883f0ce7741b6d
Ruchita Gokani <ruchitajobanputra@gmail.com>
2021-04-24 13:32:44 -0400

    Adding comment describing problem

--- running_sum.py
+++ running_sum.py
@@ -1,3 +1,10 @@
+"""
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+https://leetcode.com/problems/running-sum-of-1d-array/
+"""
+
 class Solution(object):
     def runningSum(self, nums):
         """
